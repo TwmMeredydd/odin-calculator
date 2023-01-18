5aa100424aa2fb6f5ccf45cbab7b6c13a2b9caeb
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-01-18 23:04:38 +0000

    Add operator and number functionality

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
-let n1 = 0, op="", n2 = 0;
+let n1, operator, n2, equation, resultOnDisplay;
 
-num_buttons = document.querySelectorAll("[numVal]")
+numButtons = document.querySelectorAll("[num]");
+opButtons = document.querySelectorAll("[op]");
+decimalButton = document.getElementById("decimal");
+computeButton = document.getElementById("compute");
+
+numButtons.forEach(e => e.addEventListener("click", () => handleNumber(e.getAttribute("num"))));
+opButtons.forEach(e => e.addEventListener("click", () => handleOperator(e.getAttribute("op"))));
+decimalButton.addEventListener("click", handleDecimal);
+computeButton.addEventListener("click", compute);
+
+function handleDecimal() {
+    if (operator == "") {
+        n1 += ".";
+    } else {
+        n2 += ".";
+    }
+}
+
+function handleNumber(val) {
+    if (resultOnDisplay) resetCalcuator();
+
+    if (operator == "") {
+        n1 = (n1 * 10 + +val).toString();
+    } else {
+        n2 = (n2 * 10 + +val).toString();
+    }
+    syncDisplay();
+}
+
+function handleOperator(op) {
+    if (n2 != "") {
+        compute();
+    }
+
+    operator = op;
+    resultOnDisplay = false;
+    prevEquation = [n1, op].join(" ")
+    syncDisplay();
+}
+
+function syncDisplay() {
+    document.getElementById("equation").innerText = prevEquation;
+    document.getElementById("result").innerText = operator == "" ? n1 : n2;
+}
+
+function resetCalcuator() {
+    n1 = "0";
+    operator = "";
+    n2 = "0";
+    prevEquation = "";
+    resultOnDisplay = false;
+    syncDisplay();
+}
+
+function compute() {
+    if (operator == "") return;
+
+    prevEquation = [n1, operator, n2, "="].join(" ");
+
+    let r;
+    switch (operator) {
+        case "+":
+            r = +n1 + +n2;
+            break;
+        case "-":
+            r = +n1 - +n2;
+            break;
+        case "÷":
+            r = +n1 / +n2;
+            break;
+        case "×":
+            r = +n1 * +n2;
+            break;
+    }
+
+    n1 = r.toString();
+
+    n2 = "0";
+    operator = "";
+    resultOnDisplay = true;
+    syncDisplay();
+}
+
+resetCalcuator();

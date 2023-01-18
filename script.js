ea969e9762cfb2bddb30946cf46ebc61c78555ca
TwmMeredydd <49553663+TwmMeredydd@users.noreply.github.com>
2023-01-18 23:10:00 +0000

    Connect clear button to event

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ opButtons = document.querySelectorAll("[op]");
 decimalButton = document.getElementById("decimal");
 computeButton = document.getElementById("compute");
 clearEntryButton = document.getElementById("clear-entry");
+clearButton = document.getElementById("clear");
 
 numButtons.forEach(e => e.addEventListener("click", () => handleNumber(e.getAttribute("num"))));
 opButtons.forEach(e => e.addEventListener("click", () => handleOperator(e.getAttribute("op"))));
 decimalButton.addEventListener("click", handleDecimal);
 computeButton.addEventListener("click", compute);
 clearEntryButton.addEventListener("click", handleClearEntry);
+clearButton.addEventListener("click", resetCalcuator);
 
 function handleDecimal() {
     if (operator == "") {
